4061d09015fd8a9e3465c68d7b4094e762bac367
rhojackson <Rhodesia@Rhodesias-MacBook-Pro.local>
2016-04-08 19:21:36 -0400

    Working on mail form

--- mail.php
+++ mail.php
@@ -1,10 +1,11 @@
-<?php $name = $_POST['name'];
+<?php 
+$name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "rhodesia.jackson@gmail.com";
+$recipient = "RHODESIA.JACKSON@GMAIL.COM";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+mail($recipient, $subject, $formcontent, $mailheader) or die("why won't this work?");
 echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
